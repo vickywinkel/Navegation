db1adbe934ab966a71bd909c854edc6944a22fb5
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-08-29 12:08:44 -0300

    Initial commit

---
+++ Assets/AgenteScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgenteScript : MonoBehaviour
+{
+    public Transform targetTR;
+    public Transform targetTR2;
+    public float remaining; 
+    public float minDistance;
+    public bool yendo; 
+    public NavMeshAgent navAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navAgent.remainingDistance < minDistance && navAgent.hasPath)
+        {
+            Debug.Log("Llegue : " + navAgent.remainingDistance);
+            yendo = !yendo; 
+        }
+        if (yendo)
+        {
+            navAgent.destination = targetTR.position; //le seteo hasta donde tiene que ir
+        }
+        else
+        {
+            navAgent.destination = targetTR2.position; 
+        }
+    }
+}
